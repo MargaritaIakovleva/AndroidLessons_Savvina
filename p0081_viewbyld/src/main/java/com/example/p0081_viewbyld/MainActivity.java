ae79de46ad0f8b901cfa36a5e9383cc0fcd5e2af
MargaritaIakovleva <marita05667@gmail.com>
2023-03-06 11:45:33 +0900

    6march

--- p0081_viewbyld/src/main/java/com/example/p0081_viewbyld/MainActivity.java
+++ p0081_viewbyld/src/main/java/com/example/p0081_viewbyld/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.p0081_viewbyld;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         TextView myTextView = (TextView) findViewById(R.id.myText);
+        myTextView.setText("New text in TextView");
+
+        Button myBtn = (Button) findViewById(R.id.myBtn);
+        myBtn.setText("My button");
+        myBtn.setEnabled(false);
+
+        CheckBox myChb = (CheckBox) findViewById(R.id.myChb);
+        myChb.setChecked(true);
     }
 }
