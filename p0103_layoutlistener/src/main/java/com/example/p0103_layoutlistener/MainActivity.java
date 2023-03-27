87e2c25179ab9c2d3d28922eb399cfb1f7e851d4
MargaritaIakovleva <marita05667@gmail.com>
2023-03-27 11:47:24 +0900

    27march

---
+++ p0103_layoutlistener/src/main/java/com/example/p0103_layoutlistener/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.p0103_layoutlistener;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void onClickStart(View v) {
+        // действия при нажатии на кнопку
+    };
+    btnStart.setOnClickListener(oclBtn);
+}
